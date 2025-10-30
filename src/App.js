ee974705490d5e599855897ad7e7b3657c952437
unknown <alsdnrskfk85@naver.com>
2025-10-30 17:53:16 +0900

    feat: 사용자에게 금액을 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import InputCash from "./Input-Cash.js";
+
 class App {
-  async run() {}
+  async run() {
+    const UserCash = await InputCash();
+    MissionUtils.Console.print(UserCash);
+  }
 }
 
 export default App;
