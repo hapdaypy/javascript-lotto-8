e27429984a87a895a3e692f1e87b2641d70a3b75
unknown <alsdnrskfk85@naver.com>
2025-10-30 22:18:05 +0900

    feat: 무작위 숫자 번호를 티켓처럼 만들어 배열에 저장(캡슐화)후 배열의 형태로 출력

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import InputCash from "./Input-Cash.js";
+import InputCash from "./Input-Output/Input-Cash.js";
+import CaculateTryChance from "./Caculate/Caculate-TryNumber.js";
+import LottoNumber from "./Lotto/MakeLottoArry.js";
 
 class App {
   async run() {
-    const UserCash = await InputCash();
-    MissionUtils.Console.print(UserCash);
+    const userCash = await InputCash();
+    const uesrLottoTryChance = CaculateTryChance(userCash);
+    MissionUtils.Console.print(`${uesrLottoTryChance}개를 구매했습니다.`);
+
+    const userLottoNumber = LottoNumber(uesrLottoTryChance); // 로또 번호가 담긴 새로운 배열
+
+    userLottoNumber.forEach((ticket) => {
+      const numbers = ticket;
+      MissionUtils.Console.print(`[${numbers.join(", ")}]`);
+    });
   }
 }
 
