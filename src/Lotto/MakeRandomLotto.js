31003a901b089d3017665aa07abaad6b64fdf6a6
unknown <alsdnrskfk85@naver.com>
2025-10-30 21:25:54 +0900

    feat: 6개의 변수를 생성하는 파일 생성

---
+++ src/Lotto/MakeRandomLotto.js
@@ -0,0 +1,7 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+function MakeRandomVariable() {
+  return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+}
+
+export default MakeRandomVariable;
