acbe77fbc494f0dc6ccf06229ee04e4710d5b912
unknown <alsdnrskfk85@naver.com>
2025-10-31 20:37:12 +0900

    feat:App.js에서  수익률을 계산하는 함수 불러오는 기능 추가 Caculate/Caculate-TotalRetun에 수익률을 계산하는 기능 생성.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CaculateTryChance from "./Caculate/Caculate-TryNumber.js";
 import LottoNumber from "./Lotto/MakeLottoArry.js";
 import inputWinningNumber from "./Input-Output/Input-WinningNumber.js";
 import caculateNumberOfMatch from "./Caculate/Caculate-NumberOfMatched.js";
-
+import caculateProfit from "./Caculate/Caculate-TotalReturn.js";
 class App {
   async run() {
     const userCash = await InputCash();
@@ -25,9 +25,23 @@ class App {
       winningNumber
     );
 
-    caluatedUserGetCash.forEach((item) => {
-      MissionUtils.Console.print(`[${item}]`);
-    });
+    MissionUtils.Console.print(
+      `3개 일치 (5,000원) - [${caluatedUserGetCash[0]}]개`
+    );
+    MissionUtils.Console.print(
+      `4개 일치 (50,000원) - [${caluatedUserGetCash[1]}]개`
+    );
+    MissionUtils.Console.print(
+      `5개 일치 (1,500,000원) - [${caluatedUserGetCash[2]}]개`
+    );
+    MissionUtils.Console.print(
+      `5개 일치, 보너스 볼 일치(30,000,000원) - [${caluatedUserGetCash[3]}개`
+    );
+    MissionUtils.Console.print(
+      `6개 일치 (2,000,000,000원) - [${caluatedUserGetCash[4]}]개`
+    );
+    const solution = caculateProfit(caluatedUserGetCash, userCash);
+    MissionUtils.Console.print(solution);
   }
 }
 
