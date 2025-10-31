624618aaae438d3a4fd515b298c85ade29df7e14
unknown <alsdnrskfk85@naver.com>
2025-11-01 08:00:23 +0900

    refactor: 보너스 번호를 문자로 입력 받고 있어서 보너스 번호가 포함되는지 확인할 때, 오류가 발생했음 그래서 숫자로 고쳐주는 작업 추가.

--- src/App.js
+++ src/App.js
@@ -35,11 +35,12 @@ class App {
       `5개 일치 (1,500,000원) - [${caluatedUserGetCash[2]}]개`
     );
     MissionUtils.Console.print(
-      `5개 일치, 보너스 볼 일치(30,000,000원) - [${caluatedUserGetCash[3]}개`
+      `5개 일치, 보너스 볼 일치(30,000,000원) - [${caluatedUserGetCash[3]}]개`
     );
     MissionUtils.Console.print(
       `6개 일치 (2,000,000,000원) - [${caluatedUserGetCash[4]}]개`
     );
+
     const solution = caculateProfit(caluatedUserGetCash, userCash);
     MissionUtils.Console.print(solution);
   }
