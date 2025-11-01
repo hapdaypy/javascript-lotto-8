0829249dd7cbfae6c81e46a79a0eadebc20424e6
unknown <alsdnrskfk85@naver.com>
2025-11-01 13:26:59 +0900

    feat: 사용자 로또 번호에 당첨 번호가 몇번 있었는지 출력하는 기능 파일 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LottoNumber from "./Lotto/MakeLottoArry.js";
 import inputWinningNumber from "./Input-Output/Input-WinningNumber.js";
 import caculateNumberOfMatch from "./Caculate/Caculate-NumberOfMatched.js";
 import caculateProfit from "./Caculate/Caculate-TotalReturn.js";
+import printUserCashRecord from "./Input-Output/OutPut-userCash.js";
 class App {
   async run() {
     const userCash = await InputCash();
@@ -12,34 +13,20 @@ class App {
     MissionUtils.Console.print(`${uesrLottoTryChance}개를 구매했습니다.`);
 
     const userLottoNumber = LottoNumber(uesrLottoTryChance); // 로또 번호가 담긴 새로운 배열
+
     userLottoNumber.forEach((ticket) => {
+      // 파일로 관리
       MissionUtils.Console.print(`[${ticket.lottoNumber}]`);
     });
 
     const winningNumber = await inputWinningNumber(); // 로또 번호와 보너스 번호를 입력 받음
-
     const caluatedUserGetCash = caculateNumberOfMatch(
       // 수익률을 계산하기 위한
       uesrLottoTryChance,
       userLottoNumber,
       winningNumber
     );
-
-    MissionUtils.Console.print(
-      `3개 일치 (5,000원) - [${caluatedUserGetCash[0]}]개`
-    );
-    MissionUtils.Console.print(
-      `4개 일치 (50,000원) - [${caluatedUserGetCash[1]}]개`
-    );
-    MissionUtils.Console.print(
-      `5개 일치 (1,500,000원) - [${caluatedUserGetCash[2]}]개`
-    );
-    MissionUtils.Console.print(
-      `5개 일치, 보너스 볼 일치(30,000,000원) - [${caluatedUserGetCash[3]}]개`
-    );
-    MissionUtils.Console.print(
-      `6개 일치 (2,000,000,000원) - [${caluatedUserGetCash[4]}]개`
-    );
+    printUserCashRecord(caluatedUserGetCash);
 
     const solution = caculateProfit(caluatedUserGetCash, userCash);
     MissionUtils.Console.print(solution);
