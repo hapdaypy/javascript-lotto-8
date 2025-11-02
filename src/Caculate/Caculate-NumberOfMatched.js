93ffb52778a3d3894d3c8c29cf267a9af244bf5c
unknown <alsdnrskfk85@naver.com>
2025-11-03 08:10:47 +0900

    refactor: 1. 숫자를 변수에 담아서 관리하는 식으로 번경 2. 하나의 매서드 15라인을 넘기 않기 하기 위해서 기존에 CaculateUserGetCash 함수를 getRank 함를 사용함으로 사용을 분리, getRank에서 5개를 맞추었을 때, 보너스 번호를 맞췄는지 확인하는 기능 추가함으로 bonusNumberRead함수를  삭제

--- src/Caculate/Caculate-NumberOfMatched.js
+++ src/Caculate/Caculate-NumberOfMatched.js
@@ -4,36 +4,62 @@ import { Console } from "@woowacourse/mission-utils";
 // 5등이면 0에 저장되고 4등이면 1에 저장되는식임
 // 값을 하드코딩하면 안 됨
 
-const loTTo_inpormantion = {
-  Match_Counter : {
-    TREE : 3,
-    FOUR : 4,
-    FIVE : 5,
-    SIX : 6,
+const lotto_inpormantion = {
+  Match_Counter: {
+    TREE: 3,
+    FOUR: 4,
+    FIVE: 5,
+    SIX: 6,
   },
-  
+  Lotto_Rankin_Index: {
+    FIFTH: 0, // 5등
+    FOURTH: 1,
+    THIRTH: 2,
+    SECOND: 3,
+    FIRST: 4, // 1등
+  },
+  Min_Mathch: 3,
+  Total_Rank: 5,
+};
+
+function getRank(userLottoNumberArray, userCorrecNumber, bonusNumberRead) {
+  if (userCorrecNumber < lotto_inpormantion.Min_Mathch) return null;
+  if (userCorrecNumber === lotto_inpormantion.TREE)
+    // 5등
+    return lotto_inpormantion.Lotto_Rankin_Index.FIFTH;
+  if (userCorrecNumber === lotto_inpormantion.Match_Counter.FOUR)
+    // 4등
+    return lotto_inpormantion.Lotto_Rankin_Index.FOURTH;
+  if (userCorrecNumber === lotto_inpormantion.Match_Counter.SIX)
+    // 1등 6개 맞춤
+    return lotto_inpormantion.Lotto_Rankin_Index.FIRST;
+  if (userCorrecNumber === lotto_inpormantion.Match_Counter.FIVE) {
+    const hasBounus = userLottoNumberArray.includes(bonusNumberRead);
+    if (hasBounus) {
+      return lotto_inpormantion.Lotto_Rankin_Index.SECOND;
+    }
+    return lotto_inpormantion.Lotto_Rankin_Index.THIRTH;
   }
+  return null;
 }
+
 function CaculateUseWrGetCash(tryNumber, userLottoNumber, winningNumber) {
   let caculatedGetUserCash = new Array(5).fill(0);
   const winningNumberRead = winningNumber[0].lottoNumber;
   const bonusNumberRead = Number(winningNumber[1]);
+
   for (let index = 0; index < tryNumber; index++) {
-    const userCorrecNumber = matchNumber(
-      userLottoNumber[index].lottoNumber,
-      winningNumberRead
+    const userLottoArray = userLottoNumber[index].lottoNumber;
+    const userCorrecNumber = matchNumber(userLottoArray, winningNumberRead);
+    const rankIndex = getRank(
+      userLottoArray,
+      userCorrecNumber,
+      bonusNumberRead
     );
 
-    if (userCorrecNumber < 3) continue;
-    else if (userCorrecNumber === 3) caculatedGetUserCash[0] += 1;
-    else if (userCorrecNumber === 4) caculatedGetUserCash[1] += 1;
-    else if (userCorrecNumber === 5) {
-      checkInclueBonusNumber(
-        userLottoNumber[index].lottoNumber,
-        bonusNumberRead,
-        caculatedGetUserCash
-      );
-    } else if (userCorrecNumber === 6) caculatedGetUserCash[4] += 1;
+    if (rankIndex !== null) {
+      caculatedGetUserCash[rankIndex] += 1;
+    }
   }
   return caculatedGetUserCash;
 }
@@ -43,20 +69,6 @@ function matchNumber(userLottoNumber, winningNumber) {
   let commonElements = winningNumber.filter((item) => set2.has(item));
   const correctNumber = commonElements.length;
   return correctNumber;
-}
-
-function checkInclueBonusNumber(
-  userLottoNumber,
-  BonusNumber,
-  caculatedGetUserCash
-) {
-  const tureOrFals = userLottoNumber.includes(BonusNumber);
-
-  if (tureOrFals === false) caculatedGetUserCash[2] += 1;
-  else if (tureOrFals === true) {
-    caculatedGetUserCash[3] += 1;
-  }
-  return caculatedGetUserCash;
 }
 
 export default CaculateUseWrGetCash;
