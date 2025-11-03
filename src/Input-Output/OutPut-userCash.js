925ee1e680556615425d15f61642def94c5bfe1e
unknown <alsdnrskfk85@naver.com>
2025-11-03 10:56:08 +0900

    chore: 출력 형식 문제 조건에 맞게 수정

--- src/Input-Output/OutPut-userCash.js
+++ src/Input-Output/OutPut-userCash.js
@@ -1,12 +1,16 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 function outPutUserCashRecord(caluatedUserGetCash) {
-  MissionUtils.Console.print(
-    `3개 일치 (5,000원) - ${caluatedUserGetCash[0]}개\n
-    4개 일치 (50,000원) - ${caluatedUserGetCash[1]}개\n
-    5개 일치 (1,500,000원) - ${caluatedUserGetCash[2]}개\n
-    5개 일치, 보너스 볼 일치(30,000,000원) - ${caluatedUserGetCash[3]}개\n
-    6개 일치 (2,000,000,000원) - ${caluatedUserGetCash[4]}개`
-  );
+  const outputLines = [
+    "당첨 통계",
+    "---",
+    `3개 일치 (5,000원) - ${caluatedUserGetCash[0]}개`,
+    `4개 일치 (50,000원) - ${caluatedUserGetCash[1]}개`,
+    `5개 일치 (1,500,000원) - ${caluatedUserGetCash[2]}개`,
+    `5개 일치, 보너스 볼 일치(30,000,000원) - ${caluatedUserGetCash[3]}개`,
+    `6개 일치 (2,000,000,000원) - ${caluatedUserGetCash[4]}개`,
+  ];
+
+  MissionUtils.Console.print(outputLines.join("\n"));
 }
 
 export default outPutUserCashRecord;
