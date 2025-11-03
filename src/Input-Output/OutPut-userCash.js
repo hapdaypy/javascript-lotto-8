2c67fe13d170cf6d1dea266ba2bd6a2ed3359125
unknown <alsdnrskfk85@naver.com>
2025-11-03 10:45:00 +0900

    chore: 출력 형식 간소화

--- src/Input-Output/OutPut-userCash.js
+++ src/Input-Output/OutPut-userCash.js
@@ -1,19 +1,11 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 function outPutUserCashRecord(caluatedUserGetCash) {
   MissionUtils.Console.print(
-    `3개 일치 (5,000원) - ${caluatedUserGetCash[0]}개`
-  );
-  MissionUtils.Console.print(
-    `4개 일치 (50,000원) - ${caluatedUserGetCash[1]}개`
-  );
-  MissionUtils.Console.print(
-    `5개 일치 (1,500,000원) - ${caluatedUserGetCash[2]}개`
-  );
-  MissionUtils.Console.print(
-    `5개 일치, 보너스 볼 일치(30,000,000원) - ${caluatedUserGetCash[3]}개`
-  );
-  MissionUtils.Console.print(
-    `6개 일치 (2,000,000,000원) - ${caluatedUserGetCash[4]}개`
+    `3개 일치 (5,000원) - ${caluatedUserGetCash[0]}개\n
+    4개 일치 (50,000원) - ${caluatedUserGetCash[1]}개\n
+    5개 일치 (1,500,000원) - ${caluatedUserGetCash[2]}개\n
+    5개 일치, 보너스 볼 일치(30,000,000원) - ${caluatedUserGetCash[3]}개\n
+    6개 일치 (2,000,000,000원) - ${caluatedUserGetCash[4]}개`
   );
 }
 
--- src/Input-Output/Output-userLottoNumber.js
+++ src/Input-Output/Output-userLottoNumber.js
@@ -2,7 +2,6 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 function printUserLottoNumber(userLottoNumber) {
   userLottoNumber.forEach((ticket) => {
-    // 파일로 관리
     MissionUtils.Console.print(`[${ticket.lottoNumber}]`);
   });
 }
