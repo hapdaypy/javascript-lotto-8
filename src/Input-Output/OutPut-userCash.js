a9bed5ed92b4a4698d0640890a06ef27d05d4451
unknown <alsdnrskfk85@naver.com>
2025-11-03 16:20:51 +0900

    fix: 띄어쓰기 수정

--- src/Input-Output/OutPut-userCash.js
+++ src/Input-Output/OutPut-userCash.js
@@ -1,16 +1,14 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 function outPutUserCashRecord(caluatedUserGetCash) {
-  const outputLines = [
-    "\n당첨 통계",
-    "---",
-    `3개 일치 (5,000원) - ${caluatedUserGetCash[0]}개`,
-    `4개 일치 (50,000원) - ${caluatedUserGetCash[1]}개`,
-    `5개 일치 (1,500,000원) - ${caluatedUserGetCash[2]}개`,
-    `5개 일치, 보너스 볼 일치(30,000,000원) - ${caluatedUserGetCash[3]}개`,
-    `6개 일치 (2,000,000,000원) - ${caluatedUserGetCash[4]}개`,
-  ];
-
-  MissionUtils.Console.print(outputLines.join("\n"));
+  Console.print(`당첨 통계`);
+  Console.print(`---`);
+  Console.print(`3개 일치 (5,000원) - ${caluatedUserGetCash[0]}개`);
+  Console.print(`4개 일치 (50,000원) - ${caluatedUserGetCash[1]}개`);
+  Console.print(`5개 일치 (1,500,000원) - ${caluatedUserGetCash[2]}개`);
+  Console.print(
+    `5개 일치, 보너스 볼 일치 (30,000,000원) - ${caluatedUserGetCash[3]}개`
+  );
+  Console.print(`6개 일치 (2,000,000,000원) - ${caluatedUserGetCash[4]}개`);
 }
 
 export default outPutUserCashRecord;
